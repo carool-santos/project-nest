c2180d0bc241e3a449c70569de23f7903dfb803c
carool-santos <caroline.santos@opencircle.com.br>
2021-11-25 16:55:18 -0300

    feat: organizing the modules

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
 
 @Module({
+  imports: [CoursesService],
+  controllers: [CoursesController],
   providers: [CoursesService]
 })
 export class CoursesModule {}
