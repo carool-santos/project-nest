7e83a0d296c5b82d1df9827b2c25337f88ef40b7
carool-santos <caroline.santos@opencircle.com.br>
2021-11-25 15:30:48 -0300

    feat: created service courses

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesController } from './courses/courses.controller';
 import { CoursesModule } from './courses/courses.module';
+import { CoursesService } from './courses/courses.service';
 
 @Module({
   imports: [CoursesModule],
   controllers: [AppController, CoursesController],
-  providers: [AppService],
+  providers: [AppService, CoursesService],
 })
 export class AppModule {}
---
+++ src/courses/courses.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { Course } from './entities/course.entity';
+
+@Injectable()
+export class CoursesService {
+    private courses: Course[] = [
+        {
+            id: 1,
+            name: "Fundamentos redes e internet",
+            description: "Aprender os fundamentos de redes e internet",
+            tags:['internet', 'base', 'redes'],
+        },
+    ];
+
+    findAll(){
+         return this.courses;
+    }
+
+    findOne(id: string){
+        return this.courses.find((course: Course) => course.id === Number(id));
+    }
+
+    create(createCourseDto: any){
+        this.courses.push(createCourseDto);
+    }
+
+    update (id: string, updateCourseDto: any){
+        const indexCourse = this.courses.findIndex((course: Course) => course.id === Number(id),
+        );
+        this.courses[indexCourse] = updateCourseDto;
+    }
+
+    remove(id: string){
+        const indexCourse = this.courses.findIndex((course: Course) => course.id === Number(id),
+        );
+        if(indexCourse >= 0){
+            this.courses.splice(indexCourse, 1);
+        }
+    }
+}
